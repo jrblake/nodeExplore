81342312718bdbbebcedbcf0a3b29e911a7292dc
Jared Blake <jared.r.blake@gmail.com>
2017-02-17 21:28:41 -0800

    Add Playground

---
+++ NotesNode/playground/arrow-function.js
@@ -0,0 +1,16 @@
+//arrow function
+var square = x => x*x;
+console.log(square(9));
+
+var user = {
+  name: 'Jared',
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(arguments);
+    console.log(`Hi. I'm ${this.name}`);
+  }
+};
+user.sayHi(1, 2, 3);
